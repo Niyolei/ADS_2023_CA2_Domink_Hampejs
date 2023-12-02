dd7f2aaf18c72dbe538ae2a2c1eaefc20ea53454
Niyolei <113349382+Niyolei@users.noreply.github.com>
2023-12-02 18:11:39 +0000

    Initial Commit
    Initial commit with the project file

---
+++ CA2/CA2_Test/CA2_Test.cpp
@@ -0,0 +1,16 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace CA2Test
+{
+	TEST_CLASS(CA2Test)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
